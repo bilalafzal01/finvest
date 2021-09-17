2cf2dd51560753ae2aee598245478bfe12cebd2f
bilalafzal01 <mafzal.bscs18seecs@seecs.edu.pk>
2021-09-18 01:48:36 +0500

    feat(init): basic be setup done

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,8 @@
+require('dotenv').config()
 import mongoose from 'mongoose'
 import { app } from './app'
 
 const start = async () => {
-  //   if (!process.env.JWT_KEY) {
-  //     throw new Error('JWT_KEY must be defined')
-  //   }
   if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI must be defined')
   }
@@ -13,7 +11,6 @@ const start = async () => {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true,
     })
     console.log(`Connected to AUTH mongodb!`)
   } catch (err) {
