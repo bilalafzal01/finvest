d4a5e3805a0c261d75523732d4394c8a6ed12f18
bilalafzal01 <mafzal.bscs18seecs@seecs.edu.pk>
2021-09-18 11:29:26 +0500

    feat(fix): fixed paths

--- backend/src/routes/auth/signin.js
+++ backend/src/routes/auth/signin.js
@@ -2,10 +2,10 @@ import express from 'express'
 import { body } from 'express-validator'
 import jwt from 'jsonwebtoken'
 
-import { User } from '../models/user'
-import { Password } from '../services/password'
-import { validateRequest } from '../middlewares/validate-request'
-import { BadRequestError } from '../errors/bad-request-error'
+import { User } from '../../models/user'
+import { Password } from '../../services/password'
+import { validateRequest } from '../../middlewares/validate-request'
+import { BadRequestError } from '../../errors/bad-request-error'
 
 const router = express.Router()
 
--- backend/src/routes/auth/signup.js
+++ backend/src/routes/auth/signup.js
@@ -2,10 +2,10 @@ import express from 'express'
 import { body } from 'express-validator'
 import jwt from 'jsonwebtoken'
 
-import { validateRequest } from '../middlewares/validate-request'
-import { BadRequestError } from '../errors/bad-request-error'
+import { validateRequest } from '../../middlewares/validate-request'
+import { BadRequestError } from '../../errors/bad-request-error'
 
-import { User } from '../models/user'
+import { User } from '../../models/user'
 
 const router = express.Router()
 
